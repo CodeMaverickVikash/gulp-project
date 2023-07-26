20cb50cbb6655b86a3db0883efecc62fce8a2862
vmaskhare <vmaskhare@asite.com>
2023-07-26 17:26:18 +0530

    gulp project init

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Web development with gulp tool
+
+Optimize the web performance by building bundle and minimise the load time of web.
+and setup dev and production env with sourcemap
---
+++ gulp/gulpfile.js
@@ -0,0 +1,56 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const minifyJS = require('gulp-terser'); // compressed js
+const minifyCSS = require('gulp-csso'); // compressed css
+const options = require('gulp-options');
+const sourcemaps = require('gulp-sourcemaps');
+
+const dest = '../bundles';
+const scssPath = '../webapp/sass/**/*.scss';
+const jsPath = '../webapp/js/**/*.js';
+
+gulp.task('styles', function() {
+    let tempTask = gulp.src(scssPath);
+    function process() {
+        tempTask = tempTask.pipe(sass().on('error', sass.logError))
+        .pipe(minifyCSS())
+        .pipe(concat("cssBundle" + '.css'));
+    }
+    if(!options.has('dev')) {
+        process();
+    } else {
+        tempTask = tempTask.pipe(sourcemaps.init());
+        process();
+        tempTask = tempTask.pipe(sourcemaps.write('.'))
+    }
+    return tempTask.pipe(gulp.dest(dest));
+});
+
+gulp.task('jsBuild', function(){
+    let tempTask = gulp.src(jsPath);
+    function process() {
+        tempTask = tempTask.pipe(minifyJS())
+        .pipe(concat("jsBundle" + '.js'));
+    }
+    if(!options.has('dev')) {
+        process();
+    } else {
+        tempTask = tempTask.pipe(sourcemaps.init());
+        process();
+        tempTask = tempTask.pipe(sourcemaps.write('.'))
+    }
+    return tempTask.pipe(gulp.dest(dest));
+})
+
+gulp.task('copyStatic', function(){
+    return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat("copyStatic" + '.css'))
+    .pipe(gulp.dest(dest));
+});
+
+const taskList = ['styles', 'jsBuild'];
+gulp.task('default', gulp.series([
+	gulp.parallel(taskList)
+].concat(options.has('dev') ? [] : ['copyStatic'])));
